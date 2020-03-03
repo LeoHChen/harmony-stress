681269a585418aa5080863eb9f51b4e4a2c13c83
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-03-03 20:07:14 +0700

    Update installer script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Harmony Validator Spammer + hmy"
 curl -LO https://harmony.one/hmycli && mv hmycli hmy && chmod u+x hmy
-curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-stress && chmod u+x harmony-stress
+curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/stress && chmod u+x stress
 curl -LOs https://raw.githubusercontent.com/SebastianJ/harmony-stress/master/config.yml
 curl -LOs https://raw.githubusercontent.com/SebastianJ/harmony-stress/master/staking.yml
 echo "Harmony Validator Spammer is now ready to use!"
